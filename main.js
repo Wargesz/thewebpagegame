b709699fd7fb0c0b1868110e7b604cab1a6b79e7
Wargesz <Wargesz@users.noreply.github.com>
2019-12-28 14:23:06 +0100

    asd

--- main.js
+++ main.js
@@ -9,35 +9,40 @@ let length = 8;
 let player = '0';
 let enemy = 'x';
 let space = '.';
+let gamerunner = 1;
 //
-document.addEventListener("keydown", function (event) {
-    console.log(event);
-    if (event.key == "w" && playerPosY != 1) {
-        playerPosX--;
-    }
-    if (event.key == "s" && playerPosY != height) {
-        playerPosX++;
-    }
-    if (event.key == "a" && playerPosX != 1) {
-        playerPosY--;
-    }
-    if (event.key == "d" && playerPosX != length) {
-        playerPosY++;
-    }
-});
-//
-document.addEventListener("keydown", function (event) {
-    for (let y = 1; y <= height; y++) {
-        for (let x = 1; x <= length; x++) {
-            if (x == playerPosX && y == playerPosY) {
-                document.write(player);
-            }
-
-            else if (x == enemyPosX && y == enemyPosY) {
-                document.write(enemy);
-            }
-            else document.write(space);
+gamerunner = 1;
+while (gamerunner != 2) {
+    document.addEventListener("keydown", function (event) {
+        console.log(event);
+        if (event.key == "w" && playerPosY != 1) {
+            playerPosX--;
         }
-        document.write("<br>");
-    }
-});
+        if (event.key == "s" && playerPosY != height) {
+            playerPosX++;
+        }
+        if (event.key == "a" && playerPosX != 1) {
+            playerPosY--;
+        }
+        if (event.key == "d" && playerPosX != length) {
+            playerPosY++;
+        }
+    });
+    //
+    document.addEventListener("keydown", function (event) {
+        for (let y = 1; y <= height; y++) {
+            for (let x = 1; x <= length; x++) {
+                if (x == playerPosX && y == playerPosY) {
+                    document.write(player);
+                }
+
+                else if (x == enemyPosX && y == enemyPosY) {
+                    document.write(enemy);
+                }
+                else document.write(space);
+            }
+            document.write("<br>");
+        }
+    });
+    gamerunner = 2;
+}
