cc86b499643e03feedd5aa88f2671a498f8d525e
Wargesz <Wargesz@users.noreply.github.com>
2019-12-28 14:10:23 +0100

    asd

--- main.js
+++ main.js
@@ -9,35 +9,41 @@ let length = 8;
 let player = '0';
 let enemy = 'x';
 let space = '.';
+let isMapUpdated = true;
 //
 document.addEventListener("keydown", function (event) {
     console.log(event);
     if (event.key == "w" && playerPosX != 1) {
         playerPosX--;
+        isMapUpdated = true;
     }
     if (event.key == "s" && playerPosX != height) {
-        playerPoSX++;
+        playerPosX++;
+        isMapUpdated = true;
     }
     if (event.key == "a" && playerPosY != 1) {
         playerPosY--;
+        isMapUpdated = true;
     }
     if (event.key == "d" && playerPosY != length) {
         playerPosY++;
+        isMapUpdated = true;
     }
 });
 //
-for (let y = 1; y <= height; y++)
-{
-    for (let x = 1; x <= length; x++)
-    {
-        if (x == playerPosX && y == playerPosY) {
-            document.write(player);
-        }
+if (isMapUpdated == true) {
+    for (let y = 1; y <= height; y++) {
+        for (let x = 1; x <= length; x++) {
+            if (x == playerPosX && y == playerPosY) {
+                document.write(player);
+            }
 
-        else if (x == enemyPosX && y == enemyPosY) {
-            document.write(enemy);
+            else if (x == enemyPosX && y == enemyPosY) {
+                document.write(enemy);
+            }
+            else document.write(space);
         }
-        else document.write(space);
+        document.write("<br>");
     }
-    document.write("<br>");
+    isMapUpdated = false;
 }
