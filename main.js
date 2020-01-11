e7140c4f43248df85f991e9e1f6dc45e61e72ad5
Wargesz <Wargesz@users.noreply.github.com>
2020-01-11 15:08:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,27 @@ let playerPosX = 1;
 let playerPosY = 1;
 let enemyPosX = 20;
 let enemyPosY = 20;
+let coinPosX = 0;
+let coinPosY = 0;
 let height = 20;
 let length = 20;
 let player = '0';
 let enemy = 'x';
 let space = '_';
+let coin = '$';
+let collectedCoins = 0;
 let keyPressed = false;
-let enemyHealth = 10;
 let enemyMovementNumber = null;
 let playerHasMoved = false;
 let gameRunner = true;
+let coinOnGround = false;
 //
 gameLoop();
 //
     function gameLoop() {
         setTimeout(function () {
             if (gameRunner == true) {
+                coinGenerator();
                 mapDraw();
                 playerMove();
                 enemyMove();
@@ -30,7 +35,6 @@ gameLoop();
 //
 function enemyMove() {
     if (playerHasMoved == true) {
-
         enemyMovementNumber = Math.floor(Math.random() * 2);
         console.log(enemyMovementNumber);
         if (enemyPosX == playerPosX) {
@@ -56,12 +60,17 @@ function enemyMove() {
 }
 //
 function playerMove() {
-    if (playerPosX == enemyPosX && playerPosY == enemyPosY) {
-        document.write("GAME OVER");
-        gameRunner = false;
+        if (playerPosX == enemyPosX && playerPosY == enemyPosY) {
+            document.write("GAME OVER");
+            gameRunner = false;
+    }
+    if (playerPosX == coinPosX && playerPosY == coinPosY) {
+        collectedCoins++;
+        coinOnGround = false;
     }
         keyPressed = false;
         document.addEventListener("keydown", function (event) {
+        console.log(event);
             if (keyPressed == false) {
                 if (event.key == "w" && playerPosY != 1) {
                     playerPosY--;
@@ -89,11 +98,20 @@ function playerMove() {
 //
 function mapDraw() {
     document.body.innerHTML = '';
-    document.write("player y: " + playerPosY + " , x: " + playerPosX + "<br>");
-    document.write("enemy y: " + enemyPosY + " , x: " + enemyPosX + "<br>");
+    document.write("player: 0<br>");
+    document.write("enemy: x<br>");
+    document.write("coin: $<br>");
+    document.write("player x: " + playerPosX + ", y: " + playerPosY + "<br>");
+    document.write("enemy x: " + enemyPosX + ", y: " + enemyPosY + "<br>");
+    document.write("coin x: " + coinPosX + ", y: " + coinPosY + "<br>");
+    document.write("coins: "+ collectedCoins + "<br>");
         for (let y = 1; y <= height; y++) {
             for (let x = 1; x <= length; x++) {
-                if (x == enemyPosX && y == enemyPosY) {
+                if (x == coinPosX && y == coinPosY) {
+                    document.write(coin);
+                    document.write(' ');
+                }
+                else if (x == enemyPosX && y == enemyPosY) {
                     document.write(enemy);
                     document.write(' ');
                 }
@@ -106,4 +124,19 @@ function mapDraw() {
             }
             document.write("<br>");
         }
+}
+//
+function coinGenerator() {
+    if (coinOnGround == false) {
+        coinPosX = Math.floor(Math.random() * 20) + 1
+        coinPosY = Math.floor(Math.random() * 20) + 1;
+
+        if (coinPosX == playerPosX && coinPosY == playerPosY) {
+            coinGenerator();
+        }
+        if (coinPosX == enemyPosX && coinPosY == enemyPosY) {
+            coinGenerator();
+        }
     }
+    coinOnGround = true;
+}
