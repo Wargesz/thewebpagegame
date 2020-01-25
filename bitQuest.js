deccad5b597f1af0bf6cccfa7add3e7ee32ba4a9
Wargesz <Wargesz@users.noreply.github.com>
2020-01-25 23:05:48 +0100

    asd

--- bitQuest.js
+++ bitQuest.js
@@ -9,6 +9,10 @@ function bitQuestSetup() {
     playerPosX = 2;
     playerPosY = 2;
     mapTileCounter = 0;
+    currentMap = map11;
+    gatePosY = 17;
+    gatePosX = 20;
+    playerHasMoved = true;
     //
     bitQuestGameLoop();
 }
@@ -16,6 +20,7 @@ function bitQuestSetup() {
 function bitQuestGameLoop() {
     setTimeout(function () {
         if (gameRunner == true && inGame == true) {
+            bitQuestMapChanger();
             bitQuestMapDraw();
             bitQuestPlayerMove();
             bitQuestGameLoop();
@@ -24,34 +29,61 @@ function bitQuestGameLoop() {
    
 }
 //
+function bitQuestMapChanger() {
+    if (playerHasMoved == true && currentMap == map11 &&
+        playerPosX == gatePosX && playerPosY == gatePosY) {
+        currentMap = map12
+        playerPosX = 1;
+        playerPosY = 17;
+        gatePosX = 1;
+        gatePosY = 17;
+        playerHasMoved = false;
+    }
+    if (playerHasMoved == true && currentMap == map12 &&
+        playerPosX == gatePosX && playerPosY == gatePosY) {
+        currentMap = map11
+        playerPosX = 20;
+        playerPosY = 17;
+        gatePosX = 20;
+        gatePosY = 17;
+        playerHasMoved = false;
+    }
+}
+//
 function bitQuestMapDraw() {
     mapTileCounter = 0;
     document.body.innerHTML = ' ';
     document.write("player: 0<br>");
     document.write("wall: #<br>");
+    document.write("gate: O<br>")
     document.write("-----<br>");
     document.write("player x: " + playerPosX + ", y: " + playerPosY + "<br>");
     document.write("-----<br>");
-        for (y = 1; y <= height; y++) {
-            for (x = 1; x <= length; x++) {
-                if (y == playerPosY && x == playerPosX) {
-                    document.write(player);
-                    document.write(" ");
-                    playerPosNumber = mapTileCounter; 
-                    mapTileCounter++;
-                }
-                else if (spawnMap[mapTileCounter] == 1) {
-                        document.write(wall);
-                        document.write(" ");
-                        mapTileCounter++;
-                }
-                else if (spawnMap[mapTileCounter] == 0) {
-                        document.write(space);
-                        document.write(" ");
-                        mapTileCounter++;
-                    }
+    for (y = 1; y <= height; y++) {
+        for (x = 1; x <= length; x++) {
+            if (y == playerPosY && x == playerPosX) {
+                document.write(player);
+                document.write(" ");
+                playerPosNumber = mapTileCounter;
+                mapTileCounter++;
             }
-            document.write("<br>");
+            else if (currentMap[mapTileCounter] == 1) {
+                document.write(wall);
+                document.write(" ");
+                mapTileCounter++;
+            }
+            else if (currentMap[mapTileCounter] == 0) {
+                document.write(space);
+                document.write(" ");
+                mapTileCounter++;
+            }
+            else if (currentMap[mapTileCounter] == 2) {
+                document.write(gate);
+                document.write(" ");
+                mapTileCounter++;
+            }
+        }
+        document.write("<br>");
     }
     document.write("hit space to restart<br>");
     document.write("hit esc to quit");
@@ -60,21 +92,25 @@ function bitQuestMapDraw() {
 function bitQuestPlayerUp() {
     for (y; y == 1; y++) {
         playerPosY--;
+        playerHasMoved = true;
     }
 }
 function bitQuestPlayerDown() {
     for (y; y == 1; y++) {
         playerPosY++;
+        playerHasMoved = true;
     }
 }
 function bitQuestPlayerLeft() {
     for (y; y == 1; y++) {
         playerPosX--;
+        playerHasMoved = true;
     }
 }
 function bitQuestPlayerRight() {
     for (y; y == 1; y++) {
         playerPosX++;
+        playerHasMoved = true;
     }
 }
 //
@@ -85,25 +121,25 @@ function bitQuestPlayerMove() {
         document.addEventListener("keydown", function (event) {
             if (event.key == "a" || event.key == "ArrowLeft") {
                 playerNextMoveNumber = playerPosNumber - 1;
-                if (spawnMap[playerNextMoveNumber] == 0) {
+                if (currentMap[playerNextMoveNumber] != 1) {
                     bitQuestPlayerLeft();
                 }
             }
             if (event.key == "d" || event.key == "ArrowRight") {
                 playerNextMoveNumber = playerPosNumber + 1;
-                if (spawnMap[playerNextMoveNumber] == 0) {
+                if (currentMap[playerNextMoveNumber] != 1) {
                     bitQuestPlayerRight();
                 }
             }
             if (event.key == "w" || event.key == "ArrowUp") {
                 playerNextMoveNumber = playerPosNumber - 20;
-                if (spawnMap[playerNextMoveNumber] == 0) {
+                if (currentMap[playerNextMoveNumber] != 1) {
                     bitQuestPlayerUp();
                 }
             }
             if (event.key == "s" || event.key == "ArrowDown") {
                 playerNextMoveNumber = playerPosNumber + 20;
-                if (spawnMap[playerNextMoveNumber] == 0) {
+                if (currentMap[playerNextMoveNumber] != 1) {
                     bitQuestPlayerDown();
                 }
             }
